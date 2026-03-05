32fe706472b0553e5c2873f48e2d233337817cd4
M Shanmugapriya <m.shanmugapriya@mphasis.com>
2026-03-05 15:16:01 +0530

    Changes in structure

--- Frontend-Skill-Manager-Application-main/src/app/features/dashboard/employee-dashboard/employee-dashboard.component.ts
+++ Frontend-Skill-Manager-Application-main/src/app/features/dashboard/employee-dashboard/employee-dashboard.component.ts
@@ -5,6 +5,18 @@ import { CommonModule } from '@angular/common';
   selector: 'app-employee-dashboard',
   standalone: true,
   imports: [CommonModule],
-  templateUrl:'./employee-dashboard.html'
+  templateUrl: './employee-dashboard.html',
+  styleUrls: ['./employee-dashboard.css']
 })
-export class EmployeeDashboardComponent {}
+export class EmployeeDashboardComponent {
+  userName: string = "User Name";
+  employeeId: string = "266XXXX";
+  projectName: string = "Mobile Application";
+  projectId: string = "235457567";
+
+  skills = [
+    { skillId: 101, skillName: "Java", proficiency: 4 },
+    { skillId: 102, skillName: "Angular", proficiency: 3 },
+    { skillId: 103, skillName: "SQL", proficiency: 5 }
+  ];
+}
